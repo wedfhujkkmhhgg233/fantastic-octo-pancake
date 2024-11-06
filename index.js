8122efcdef241fd95b1a44596af974af26f77b2e
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2024-11-06 18:48:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -112,6 +112,9 @@ app.get('/service-list', async (req, res) => {
 import { router as bingRouter } from './api/bing.js';
 app.use('/service/api', bingRouter);
 
+import { router as brainRouter } from './api/brain.js';
+app.use('/service/api', brainRouter);
+
 import { router as catgptRouter } from './api/catgpt.js';
 app.use('/service/api', catgptRouter);
 

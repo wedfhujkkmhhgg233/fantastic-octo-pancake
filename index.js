d7bed644bd7f62585681d08287a1f7a8d9264288
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2024-11-09 15:27:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -112,6 +112,9 @@ app.get('/service-list', async (req, res) => {
 import { router as bingRouter } from './api/bing.js';
 app.use('/service/api', bingRouter);
 
+import { router as peakpxRouter } from './api/peakpx.js';
+app.use('/service/api', peakpxRouter);
+
 import { router as prodiaRouter } from './api/prodia.js';
 app.use('/service/api', prodiaRouter);
 

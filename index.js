143fc9fddd1822b476b3abf422285e4328ab1345
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2024-11-09 13:36:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -112,6 +112,9 @@ app.get('/service-list', async (req, res) => {
 import { router as bingRouter } from './api/bing.js';
 app.use('/service/api', bingRouter);
 
+import { router as prodiaRouter } from './api/prodia.js';
+app.use('/service/api', prodiaRouter);
+
 import { router as removebgv2Router } from './api/removebgv2.js';
 app.use('/service/api', removebgv2Router);
 

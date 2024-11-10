d25fc20bc9a884a8be2741094f93d58a2dd38cef
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2024-11-10 08:17:05 +0800

    Update mainjerjarjamismiss.js

--- mainjerjarjamismiss.js
+++ mainjerjarjamismiss.js
@@ -112,6 +112,9 @@ app.get('/service-list', async (req, res) => {
 import { router as bingRouter } from './api/bing.js';
 app.use('/service/api', bingRouter);
 
+import { router as redmondRouter } from './api/redmond.js';
+app.use('/service/api', redmondRouter);
+
 import { router as midjourneyRouter } from './api/midjourney.js';
 app.use('/service/api', midjourneyRouter);
 

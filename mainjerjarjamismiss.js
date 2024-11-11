f01bfd0091409ab2c67e35bc2f65f2a9f8bea912
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2024-11-11 19:24:54 +0800

    Update mainjerjarjamismiss.js

--- mainjerjarjamismiss.js
+++ mainjerjarjamismiss.js
@@ -112,6 +112,9 @@ app.get('/service-list', async (req, res) => {
 import { router as bingRouter } from './api/bing.js';
 app.use('/service/api', bingRouter);
 
+import { router as factRouter } from './api/fact.js';
+app.use('/service/api', factRouter);
+
 import { router as mealRouter } from './api/meal.js';
 app.use('/service/api', mealRouter);
 

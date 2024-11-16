749a7fe52f7df3fc91601655dc077aeb68314e88
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2024-11-16 10:01:56 +0800

    Update mainjerjarjamismiss.js

--- mainjerjarjamismiss.js
+++ mainjerjarjamismiss.js
@@ -112,6 +112,9 @@ app.get('/service-list', async (req, res) => {
 import { router as bingRouter } from './api/bing.js';
 app.use('/service/api', bingRouter);
 
+import { router as puppeteerRouter } from './api/puppeteer.js';
+app.use('/service/api', puppeteerRouter);
+
 import { router as emojiRouter } from './api/emoji.js';
 app.use('/service/api', emojiRouter);
 

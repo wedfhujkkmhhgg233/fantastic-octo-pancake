1348077a1b687403c322ad11f1c3265e8394d714
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2024-11-17 09:04:59 +0800

    Update mainjerjarjamismiss.js

--- mainjerjarjamismiss.js
+++ mainjerjarjamismiss.js
@@ -112,6 +112,9 @@ app.get('/service-list', async (req, res) => {
 import { router as bingRouter } from './api/bing.js';
 app.use('/service/api', bingRouter);
 
+import { router as catfactRouter } from './api/catfact.js';
+app.use('/service/api', catfactRouter);
+
 import { router as dogimageRouter } from './api/dogimage.js';
 app.use('/service/api', dogimageRouter);
 
